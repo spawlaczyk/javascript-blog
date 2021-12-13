dcb9c01aa713df718d44c1dc112d93a70536f7be
Stanisław Pawlaczyk <ctahncnab@gmail.com>
2021-12-13 13:32:27 +0100

    Add loop for aech article

--- js/script.js
+++ js/script.js
@@ -43,6 +43,32 @@ console.log(targetArticle);
 
 targetArticle.classList.add("active");
 
+const optArticleSelector = ".post",
+      optTitleSelector = ".post-title",
+      optTitleListSelector = ".titles";
+
+function generateTitleLinks(){
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+
+  titleList.innerHTML = "";
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+}
+
+generateTitleLinks();
+
 }
 
 const links = document.querySelectorAll(".titles a");
