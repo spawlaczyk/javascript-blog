28c2794255deb33838059b5bf82bec6d3f2c1be1
Stanisław Pawlaczyk <ctahncnab@gmail.com>
2021-12-15 14:55:11 +0100

    Fix left column problem

--- js/script.js
+++ js/script.js
@@ -1,68 +1,79 @@
 "use strict";
 
-const titleClickHandler = function (event){
+const optArticleSelector = ".post",
+  optTitleSelector = ".post-title",
+  optTitleListSelector = ".titles";
+
+const titleClickHandler = function(event) {
   event.preventDefault();
   const clickedElement = this;
   console.log("Link was clicked!");
 
- /* remove class "active" from all article links */
+  /* remove class "active" from all article links */
 
-const activeLinks = document.querySelectorAll(".titles a.active");
+  const activeLinks = document.querySelectorAll(".titles a.active");
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove("active");
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("active");
+  }
+
+  /* add class "active" to the clicked link */
+
+  clickedElement.classList.add("active");
+
+  console.log("clickedElement: ", clickedElement);
+
+  /* remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll(".posts article.active");
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
+
+  /* get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute("href");
+
+  console.log(articleSelector);
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+
+  console.log(targetArticle);
+
+  /* add class 'active' to the correct article */
+
+  targetArticle.classList.add("active");
 }
- 
- /* add class "active" to the clicked link */
 
-clickedElement.classList.add("active");
 
-console.log("clickedElement: ", clickedElement);
-
- /* remove class 'active' from all articles */
-
-const activeArticles = document.querySelectorAll(".posts article.active");
-
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove("active");
-}
-
- /* get 'href' attribute from the clicked link */
-
-const articleSelector = clickedElement.getAttribute("href");
-
-console.log(articleSelector);
-
- /* find the correct article using the selector (value of 'href' attribute) */
-
-const targetArticle = document.querySelector(articleSelector);
-
-console.log(targetArticle);
-
- /* add class 'active' to the correct article */
-
-targetArticle.classList.add("active");
-
-const optArticleSelector = ".post",
-      optTitleSelector = ".post-title",
-      optTitleListSelector = ".titles";
-
-function generateTitleLinks(){
+function generateTitleLinks() {
   const titleList = document.querySelector(optTitleListSelector);
+  console.log(titleList);
   titleList.innerHTML = "";
   const articles = document.querySelectorAll(optArticleSelector);
-  for(let artcile of articles){
+
+  let html = "";
+
+  for (let article of articles) {
     const articleId = article.getAttribute("id");
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll(".titles a");
+  console.log(links);
+
+  for (let link of links) {
+    link.addEventListener("click", titleClickHandler);
   }
 
 }
 
 generateTitleLinks();
-
-}
-
-const links = document.querySelectorAll(".titles a");
-
-for(let link of links){
-  link.addEventListener("click", titleClickHandler);
-}
